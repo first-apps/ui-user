89f02bf9c62878bd06084d0d33de2fe89cdf4bcd
dikshit-n <62920150+dikshit-n@users.noreply.github.com>
2025-01-04 12:36:52 +0530

    @first-apps/shared integration

--- app/components/PostContainer.tsx
+++ app/components/PostContainer.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@first-apps/shared/components";
+
 export const PostContainer = ({ data, children }) => {
   const handleClick = () => {
     console.log(data);
@@ -8,5 +10,10 @@ export const PostContainer = ({ data, children }) => {
     // such as add to cart
   };
   // const shouldRender = localStorage.getItem("test");
-  return <div onClick={handleClick}>{children}</div>;
+  return (
+    <div>
+      <h2>{children}</h2>
+      <Button onClick={handleClick}>Print</Button>
+    </div>
+  );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   // const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await data.json();
-  console.log(posts);
 
   return (
     <div>
